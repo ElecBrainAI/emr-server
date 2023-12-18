186e2b40081197219209585aeff3c09089e7c78d
hyunwook7120 <jhw030217@gmail.com>
2023-12-18 14:23:13 +0900

    final2

--- emr-v/src/router/index.js
+++ emr-v/src/router/index.js
@@ -1,3 +1,6 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 import SignUpView from "../views/SignUpView.vue";
 import PatientView from "../views/PatientView.vue";
 import ReceptionView from "../views/ReceptionView.vue";
